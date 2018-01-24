d21f8f82ae1805eb42312330ce55f7ef2f3c09ba
叶信节 <yexinjie@souche.com>
2018-01-24 14:49:37 +0800

    add mathLeaner

---
+++ src/main/java/com/helloxin/lang/MathLeaner.java
@@ -0,0 +1,47 @@
+package com.helloxin.lang;
+
+/**
+ * Created by nandiexin on 2018/1/23.
+ */
+public class MathLeaner {
+
+
+    public static void main(String[] args) {
+
+        //圆周率
+        System.out.println("Math.PI:"+Math.PI);
+        //这个一般好像用不到
+        System.out.println("Math.E:"+Math.E);
+
+        System.out.println("Math.abs:"+Math.abs(-10));
+
+        //这边里面的计算取得是 strictMath 的值
+        System.out.println("Math.acos:"+Math.acos(45));
+
+        System.out.println("Math.addExact:"+Math.addExact(10,10));
+
+        System.out.println("Math.cbrt:"+Math.cbrt(34.12));
+
+        System.out.println("Math.ceil:"+Math.ceil(10.12));
+
+        System.out.println("Math.max:"+Math.max(10,20));
+
+        System.out.println("Math.multiplyExact:"+Math.multiplyExact(10,15));
+
+        System.out.println("Math.multiplyFull:"+Math.multiplyFull(10,15));
+
+        System.out.println("Math.multiplyHigh:"+Math.multiplyHigh(10,15));
+
+        System.out.println("Math.negateExact:"+Math.negateExact(89));
+
+        System.out.println("Math.negateExact:"+Math.random());
+
+        System.out.println("Math.negateExact:"+Math.toIntExact(90));
+
+        System.out.println("StrictMath.tan:"+StrictMath.tan(10));
+
+
+
+    }
+
+}
