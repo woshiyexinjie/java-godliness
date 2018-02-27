7f0374885cfa5fc506c9dfd37a8760b8133d9011
叶信节 <yexinjie@souche.com>
2018-02-27 18:51:01 +0800

    add base64

---
+++ src/main/java/com/helloxin/util/Base64Leaner.java
@@ -0,0 +1,37 @@
+package com.helloxin.util;
+
+import java.util.Base64;
+
+/**
+ * Created by nandiexin on 2018/2/8.
+ */
+public class Base64Leaner {
+
+
+    public void base64Test(){
+
+        String msg = "Hello xin, Base64!";
+        Base64.Encoder enc = Base64.getEncoder();
+        byte[] encbytes = enc.encode(msg.getBytes());
+        for (int i = 0; i < encbytes.length; i++)
+        {
+            System.out.printf("%c", (char) encbytes[i]);
+            if (i != 0 && i % 4 == 0) {
+                System.out.print(' ');
+            }
+        }
+        System.out.println();
+        Base64.Decoder dec = Base64.getDecoder();
+        byte[] decbytes = dec.decode(encbytes);
+        System.out.println(new String(decbytes));
+
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        Base64Leaner base64Leaner = new Base64Leaner();
+        base64Leaner.base64Test();
+    }
+}
---
+++ src/test/java/com/helloxin/Hello.java
@@ -0,0 +1,14 @@
+package com.helloxin;
+
+/**
+ * Created by nandiexin on 2018/2/26.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+
+        String t = null;
+        String tt = t;
+        System.out.println("null".equals(tt));
+    }
+}
