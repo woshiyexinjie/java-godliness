899c5640297205defe637b2f98ad03e5e43c3139
叶信节 <yexinjie@souche.com>
2018-10-16 21:22:23 +0800

    ADD character leaner demo

--- src/main/java/com/helloxin/base/CharacterLenaer.java
+++ src/main/java/com/helloxin/base/CharacterLenaer.java
@@ -10,9 +10,27 @@ public class CharacterLenaer {
 
 //        Character ch = 'A';
 //        System.out.println(Character.charCount(97));
+//        System.out.println(Character.digit('A',3));
+//        System.out.println(Character.getNumericValue('A'));
+//        System.out.println(Character.isDefined('A'));
 
-        char array[] = {'A','s','d','f','e'};
-        System.out.println(Character.codePointAt(array,0));
+
+//        char array[] = {'A','s','d','f','e'};
+//        System.out.println(Character.codePointAt(array,0));
+
+
+//         Character cache[] = new Character[127 + 1];
+////         for (int i = 0; i < cache.length; i++) {
+////             cache[i] = new Character((char)i);
+////             System.out.println(cache[i]);
+////         }
+
+
+        char ch= 'A';
+        System.out.println(ch > 60);
+
+
+
 
 
     }
